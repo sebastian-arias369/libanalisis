60126dfc9a86c7426705e7b439e5ab5ffda4224d
Juan Salgado <juanjosesalgaperez@gmail.com>
2023-11-23 22:13:03 -0500

    En el .h se creo los identificadores y la funcion linearFit que toma como argumentos las variables, los cuales son los arreglos, puntos dados y punteros.

--- libanalisis2/linear_fit.h
+++ libanalisis2/linear_fit.h
@@ -1,28 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.h to edit this template
- */
-
-/* 
- * File:   linear_fit.h
- * Author: arias
- *
- * Created on 23 de noviembre de 2023, 10:04 p. m.
- */
-
 #ifndef LINEAR_FIT_H
 #define LINEAR_FIT_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+void linearFit(double x[], double y[], int n, double* m, double* b);
 
-
-
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* LINEAR_FIT_H */
-
+#endif /* LINEAR_FIT_H
