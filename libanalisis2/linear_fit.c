9814df675d01d2a942f03896729d303cd59e77b5
Santiago Vanegas <luis.vanegas01@uceva.edu.co>
2023-11-23 22:21:35 -0500

    Se creó un ciclo for para realización de los correspondientes calculos, y a medida que el ciclo va transcurriendo se van imprimiendo los valores de la tabla

--- libanalisis2/linear_fit.c
+++ libanalisis2/linear_fit.c
@@ -6,6 +6,18 @@ void linearFit(double x[], double y[], int n, double* m, double* b) {
     double sumY = 0.0;
     double sumXX = 0.0;
     double sumXY = 0.0;
+    
+    for (int i = 0; i < n; i++) {
+        sumX += x[i];
+        sumY += y[i];
+        sumXX += x[i] * x[i];
+        sumXY += x[i] * y[i];
+
+        // Imprimir datos de la tabla
+        printf("%2d   %8lf  %8lf  %8lf  %8lf  %8lf  %8lf  %8lf  %8lf\n",
+               i, x[i], y[i], x[i] * x[i], x[i] * y[i], sumX, sumY, sumXX, sumXY);
+    }
+
 
 }
 
